ac084a3d28fcb80f4f1c8a1dcc1207b7a197a997
Erling Tokheim <Erling Tokheim>
2016-11-21 17:01:43 +0100

    16 Cat Clicker: REDESIGN: Beautifulize with the beauty of CSS

--- 16 Cat clicker/js/javascript.js
+++ 16 Cat clicker/js/javascript.js
@@ -4,7 +4,6 @@ var clickCounter = 0;
 $("#click_counter").text(clickCounter);
 
 $("#cat_container").click(function() {
-	console.log("Cat is clicked");
 	clickCounter++;
 	$("#click_counter").text(clickCounter);
 });
