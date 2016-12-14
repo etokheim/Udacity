e7efd36daeabb18a6e8cadb9268b24db084698b4
Erling Tokheim <e@tokheim.no>
2016-12-14 19:15:11 +0100

    Update _makeindex.sh, still not working

--- _makeindex.sh
+++ _makeindex.sh
@@ -1,4 +1,50 @@
 #!/bin/sh
 
-ls -R | egrep '(jpg|png)' | \\ #filter the content you want in the index
-perl -e 'print "<html><body><ul>"; while(<>) { chop $_; print "<li><a href=\"./$_\">$_</a></li>";} print "</ul></body></html>"' > index.html
+# TEST="find . -name \*.html -print"
+TEST="$(find . -name \*.html -print)"
+
+# echo $TEST
+
+# echo $TEST > index.html
+# echo "" > index.html
+
+# array=()
+# while IFS=  read -r -d $'\0'; do
+#     array+=("$REPLY")
+#  	echo ("$REPLY") >> index.html
+# done < <(find . -name \*.html -print)
+
+# array=()
+# while IFS="/"  read -r -d $'\0'; do
+#     array+=("$REPLY")
+# done < <(find . -name ${input} -print0)
+
+
+# Working but separates by "/"
+array=()
+number=0
+while IFS='/' read -ra ADDR; do
+     for i in "${ADDR[@]}"; do
+         # process "$i"
+         number+=1
+         echo $number
+         array[number]+="$i"
+     done
+done <<< "$TEST"
+
+echo ${array[1]}
+
+
+# ls | egrep '(html)' | \
+# perl -e 'print " $TEST <html>
+# 	<body>
+# 		<ul>"; while(<>) { chop $_; print "
+# 			<li>
+# 				<a href=\"./$_\">
+# 					$_
+# 				</a>
+# 			</li>";} print "
+# 		</ul>
+# 	</body>
+# </html>"' > index.html
+
